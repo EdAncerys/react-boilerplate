32a82abc658d888fb713c0e7729dcacd631467c9
Skylark <lookatemail@gmail.com>
2021-10-31 17:09:27 +0000

    Add index sample page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './css/App.css';
 import { AppProvider } from './context';
 
 import { FrontPage } from './components/FrontPage';
+import { Index } from './screens/index';
 
 export const App = () => {
   return (
     <AppProvider>
-      <FrontPage />
+      {/* <FrontPage /> */}
+      <Index />
     </AppProvider>
   );
 };
