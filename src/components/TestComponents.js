2612082f962385b9048fa0eb6faab773623ad144
Skylark <lookatemail@gmail.com>
2021-11-01 20:17:22 +0000

    Add Bootstrap config to web app

--- src/components/TestComponents.js
+++ src/components/TestComponents.js
@@ -22,7 +22,11 @@ export const ComponentTwo = () => {
 export const Home = () => {
   return (
     <div>
-      <h2>Home Component</h2>
+      <h2 className="text-muted primary">Home Component</h2>
+      <br />
+      <button type="button" className="btn btn-lg btn-primary w-25 mx-0 mb-2">
+        Save changes
+      </button>
     </div>
   );
 };
